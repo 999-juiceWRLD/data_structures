e0f7a43b8d260702e2e5a4ead0941c3873635001
juice wrld <ankaraunifedaisi@gmail.com>
2025-08-16 00:18:13 +0300

    organize docstrings

--- data_structures/node.py
+++ data_structures/node.py
@@ -1,6 +1,7 @@
+"""This module contains basic Node classes to construct data structures."""
+
 class NodeCore:
-    """
-    This is the fundamental class that's used & inherited for linear data structures.
+    """This is the fundamental class that's used & inherited for linear data structures.
     
     Attributes:
         value (any): Value (data) stored in node.
@@ -8,14 +9,18 @@ class NodeCore:
     """
     
     def __init__(self, value, next_node=None):
+        """Initialize a NodeCore object.
+        
+        Args:
+            value (Any, optional): First value to add to the list. Defaults to None.
+            next_node (NodeCore, optional): Node class used to create list nodes. Defaults to Node.
+        """
         self._value = value
         self._next_node = next_node
         
     @property
     def value(self):
-        """
-        Any: The stored value in the `Node` object.
-        """
+        """Any: The stored value in the `Node` object."""
         return self._value
     
     @property
@@ -25,8 +30,7 @@ class NodeCore:
 
     @next_node.setter
     def next_node(self, node):
-        """
-        Set the next node.
+        """Set the next node.
         
         Args:
             node (Node | None): new `Node` object or None if it's the tail node.
@@ -34,8 +38,7 @@ class NodeCore:
         self._next_node = node
 
 class NodePriorityQueue(NodeCore):
-    """
-    This is the base node used to construct a priority queue. Inherits the NodeCore.
+    """This is the base node used to construct a priority queue. Inherits the NodeCore.
     
     Attributes:
         value (any): Value (data) stored in node.
@@ -44,12 +47,18 @@ class NodePriorityQueue(NodeCore):
     """
     
     def __init__(self, value, priority=None, next_node=None):
+        """Initialize a NodePriorityQueue object. This is used for creating priority queues. Inherits NodeCore.
+        
+        Args:
+            value (Any, optional): First value to add to the list. Defaults to None.
+            next_node (type, optional): Node class used to create list nodes. Defaults to Node.
+            priority (int): The priority value for the node. This will set the position of the node in the list.
+        """
         super().__init__(value, next_node)
         self.priority = priority
 
 class TreeNode:
-    """
-    This is the base node used when implementing binary search trees.
+    """This is the base node used when implementing binary search trees.
     
     Args:
         value (Any): The stored value in the node.
@@ -59,6 +68,14 @@ class TreeNode:
     """
     
     def __init__(self, value=None, left_child=None, right_child=None, parent=None):
+        """Initialize a TreeNode object. Used in data_structures.BinarySearchTree.
+        
+        Args:
+            value (Any): The stored value in the node.
+            left_child (Node | None): Left child of the current node.
+            right_child (Node | None): Right child of the current node.
+            parent (Node | None): Parent node of the current node.
+        """
         self._value = value
         self._left_child = left_child
         self._right_child = right_child
@@ -66,15 +83,12 @@ class TreeNode:
         
     @property
     def value(self):
-        """
-        Any: The stored value in the `TreeNode` object.
-        """
+        """Any: The stored value in the `TreeNode` object."""
         return self._value
     
     @value.setter
     def value(self, value):
-        """
-        Set a new value for the node.
+        """Set a new value for the node.
         
         Args:
             value (Any): The new value of the node.
@@ -83,15 +97,12 @@ class TreeNode:
     
     @property
     def left_child(self):
-        """
-        TreeNode | None: Returns left child the TreeNode object if exists.
-        """
+        """None | TreeNode: Returns left child the TreeNode object if exists."""
         return self._left_child
     
     @left_child.setter
     def left_child(self, left_child):
-        """
-        Set the left_child of the TreeNode.
+        """Set the left_child of the TreeNode.
         
         Args:
             left_child (TreeNode | None): Left child of the node object.
@@ -100,15 +111,12 @@ class TreeNode:
         
     @property
     def right_child(self):
-        """
-        TreeNode: Returns the right child TreeNode object.
-        """
+        """TreeNode: Returns the right child TreeNode object."""
         return self._right_child
     
     @right_child.setter
     def right_child(self, right_child):
-        """
-        Set the right_child of the TreeNode.
+        """Set the right_child of the TreeNode.
         
         Args:
             right_child (TreeNode | None): Right child of the node object.
@@ -117,29 +125,22 @@ class TreeNode:
     
     @property
     def left_value(self):
-        """
-        Any: Value of the left_child node.
-        """
+        """Any: Value of the left_child node."""
         return self._left_child.value if self._left_child else None
     
     @property
     def right_value(self):
-        """
-        Any: Value of the right_child node.
-        """
+        """Any: Value of the right_child node."""
         return self._right_child.value if self._right_child else None
     
     @property
     def parent(self):
-        """
-        TreeNode | None: Returns the parent node (TreeNode) object if exists.
-        """
+        """None | TreeNode: Returns the parent node (TreeNode) object if exists."""
         return self._parent
 
     @parent.setter
     def parent(self, parent):
-        """
-        Set the parent node.
+        """Set the parent node.
         
         Args:
             parent (TreeNode | None): Parent of the node object.
