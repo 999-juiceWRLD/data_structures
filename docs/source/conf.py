cc4882ece87789e0f9d0e8de0fd5988507700a50
juice wrld <ankaraunifedaisi@gmail.com>
2025-08-16 21:34:51 +0300

    delete and re-create docs

---
+++ docs/source/conf.py
@@ -0,0 +1,60 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'data_structures'
+copyright = '2025, Emre Kahraman CANIKOGLU'
+author = 'Emre Kahraman CANIKOGLU'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
+
+# Add these extensions
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',  # For Google-style docstrings
+    'sphinx_autodoc_typehints'
+]
+
+# Set theme (optional but recommended)
+html_theme = 'sphinx_rtd_theme'
+
+# Add your project to the Python path
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))  # Adjust based on your project structure
+
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# Make Sphinx generate correct URLs on GitHub Pages
+html_baseurl = "https://999-juicewrld.github.io/data_structures/"
+html_theme_options = {
+    "style_external_links": True,
+}
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. data_structures documentation master file, created by
-   sphinx-quickstart on Sat Aug 16 21:01:43 2025.
+   sphinx-quickstart on Sat Aug 16 21:29:49 2025.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
