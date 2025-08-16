c4c2fae17469d0a0383f2b4a2164e67dc99228c9
juice wrld <ankaraunifedaisi@gmail.com>
2025-08-16 21:44:46 +0300

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,47 +6,47 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'data_structures'
-copyright = '2025, Emre Kahraman CANIKOGLU'
-author = 'Emre Kahraman CANIKOGLU'
-release = '1.0.0'
+project = "data_structures"
+copyright = "2025, Emre Kahraman CANIKOGLU"
+author = "Emre Kahraman CANIKOGLU"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Add these extensions
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',  # For Google-style docstrings
-    'sphinx_autodoc_typehints'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.githubpages",
 ]
 
-# Set theme (optional but recommended)
-html_theme = 'sphinx_rtd_theme'
 
 # Add your project to the Python path
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))  # Adjust based on your project structure
+sys.path.insert(0, os.path.abspath("../.."))  # Adjust based on your project structure
 
 # Autodoc settings
 autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
-    'special-members': '__init__',
-    'undoc-members': True,
-    'exclude-members': '__weakref__'
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__"
 }
+autosummary_generate = True
 
 import sphinx_rtd_theme
 
@@ -57,4 +57,9 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_baseurl = "https://999-juicewrld.github.io/data_structures/"
 html_theme_options = {
     "style_external_links": True,
+    "collapse_navigation": False,
+    "navigation_depth": 4,
+    "sticky_navigation": True,
+    "titles_only": False,
 }
+html_relative_permalinks = True
